86bfcd676021d64d42db83557e3d398624c62638
Rodney Elliott <rodney.elliott@gmail.com>
2014-07-29 09:35:36 +1200

    CONVERT_QUANDL added.
    1.  Added the function CONVERT_QUANDL.
    2.  Added the test unit TEST_CONVERT_QUANDL.
    3.  Added the test unit reference data.

---
+++ Unit Tests/test_convert_quandl.m
@@ -0,0 +1,87 @@
+function tests = test_convert_quandl
+% TEST_CONVERT_QUANDL Test unit for CONVERT_QUANDL.
+%   TESTS = TEST_CONVERT_QUANDL creates an array of handles to local
+%   CONVERT_QUANDL test functions.
+%
+%   Use:
+%       results = runtests('test_convert_quandl.m')
+%
+%   See also CONVERT_QUANDL.
+
+%% File and license information.
+%**************************************************************************
+%
+%   File:           test_convert_quandl.m
+%   Module:         Input Analysis
+%   Project:        Portfolio Optimisation
+%   Workspace:      Finance Tools
+%
+%   Author:         Rodney Elliott
+%   Date:           29 July 2014
+%
+%**************************************************************************
+%
+%   Copyright:      (c) 2014 Rodney Elliott
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this program. If not, see <http://www.gnu.org/licenses/>.
+%
+%**************************************************************************
+
+%% Create test array.
+% The following code is MATLAB test unit boilerplate, and should not be
+% modified.
+tests = functiontests(localfunctions);
+end
+
+%% Local functions.
+% The first test function records the current directory, then moves up one
+% level to where CONVERT_QUANDL should be located.
+function setupOnce(testCase)
+testCase.TestData.origPath = pwd;
+cd ../
+end
+
+%%
+% The following group of test functions verify that CONVERT_QUANDL throws
+% the correct error when passed invalid input arguments.
+function test_no_argument(testCase)
+actual = @()convert_quandl();
+expected = 'MATLAB:minrhs';
+verifyError(testCase, actual, expected);
+end
+
+function test_symbol_invalid_class(testCase)
+actual = @()convert_quandl(0);
+expected = 'convert_quandl:invalid_type';
+verifyError(testCase, actual, expected);
+end
+
+%%
+% The following test function verifies that CONVERT_QUANDL returns the
+% correct set of data.
+function test_1(testCase)
+document = xmlread('test_convert_quandl.xml');
+actual = convert_quandl(document);
+expected = load('test_convert_quandl.mat', 'test_1');
+verifyEqual(testCase, actual, expected.test_1);
+end
+
+%%
+% The last test function returns to what was the current directory, and
+% removes all of the temporary variables used by the test unit from the
+% MATLAB workspace.
+function teardownOnce(testCase)
+cd(testCase.TestData.origPath);
+clear all;
+end
---
+++ convert_quandl.m
@@ -0,0 +1,115 @@
+function datasets = convert_quandl(document)
+% CONVERT_QUANDL Convert Quandl XML search results.
+%   DATASETS = CONVERT_QUANDL(DOCUMENT) converts Quandl XML search results
+%   from a document object model (DOM) node to a MATLAB structure. Field
+%   names and the nesting of structures in DATASETS match the node names
+%   and relationships in DOCUMENT.
+%
+%   Example:
+%       query = 'http://www.quandl.com/api/v1/datasets.xml?query=IBM';
+%       document = xmlread(query, 'Timeout', 10);
+%       datasets = CONVERT_QUANDL(document);
+%
+%   See also TEST_CONVERT_QUANDL.
+
+%% File and license information.
+%**************************************************************************
+%
+%   File:           convert_quandl.m
+%   Module:         Input Analysis
+%   Project:        Portfolio Optimisation
+%   Workspace:      Finance Tools
+%
+%   Author:         Rodney Elliott
+%   Date:           29 July 2014
+%
+%**************************************************************************
+%
+%   Copyright:      (c) 2014 Rodney Elliott
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this program. If not, see <http://www.gnu.org/licenses/>.
+%
+%**************************************************************************
+
+%% Parse input.
+% CONVERT_QUANDL accepts a single input argument. Because the input
+% argument contains data that is not easily decoded or checked, the
+% 'inputParser' class and its methods are not used here.
+if ~isa(document, 'org.apache.xerces.dom.DeferredDocumentImpl')
+    error('convert_quandl:invalid_type', ...
+        ['Invalid type. Function argument must be a valid document ' ...
+        'object model (DOM) node.']);
+end
+
+%% Convert search results.
+% Having located the root node of the DOM, child nodes are converted using
+% the local function CONVERT_NODE. This operates recursively, starting at
+% the root node and working its way down through the XML tree. Notice how
+% CONVERT_NODE uses the attributes of a child node, in addition to the
+% number of child nodes of the child node (aka grandchildren) to process
+% the DOM.
+root_node = document.getDocumentElement;
+datasets = convert_node(root_node);
+
+end
+
+%% Local functions.
+function s = convert_node(node)
+children = node.getChildNodes;
+
+for i = 1:children.getLength
+    child = children.item(i - 1);
+
+    if child.getNodeType == 1
+        child_name = char(child.getNodeName);
+        child_name = matlab.lang.makeValidName(child_name);
+
+        if child.hasAttributes
+            attributes = child.getAttributes;
+            
+            if strcmp(char(attributes.item(0).getValue), 'array')
+                s.(child_name) = convert_node(child);
+            elseif strcmp(char(attributes.item(0).getValue), 'boolean')
+                s.(child_name) = char(child.item(0).getData);
+            elseif strcmp(char(attributes.item(0).getValue), 'integer')
+                s.(child_name) = str2double(char(child.item(0).getData));
+            else
+                s.(child_name) = '';
+            end
+        else
+            grandchildren = child.getChildNodes;
+            
+            if grandchildren.getLength > 2
+                if exist('s', 'var')
+                    row = length(s.(child_name));
+                    s.(child_name)(row + 1) = convert_node(child);
+                else
+                    s.(child_name) = convert_node(child);
+                end
+            else
+                if exist('s', 'var') && isfield(s, child_name)
+                    row = size(s, 2);
+                    s(row + 1).(child_name) = char(child.item(0).getData);
+                else
+                    try
+                        s.(child_name) = char(child.item(0).getData);
+                    catch
+                        s.(child_name) = '';
+                    end
+                end
+            end
+        end
+    end
+end
+end
